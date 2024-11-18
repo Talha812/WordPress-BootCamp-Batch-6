a5364755cac383629da39a18851af3e6295a4d11
Muhammad Talha <72699916+Talha812@users.noreply.github.com>
2024-11-18 21:01:44 +0500

    Create header.php

---
+++ PHP Basics/header.php
@@ -0,0 +1,13 @@
+<!-- Code with CSS -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My WordPress Site</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h2>Header: Welcome to My WordPress Site</h2>
+    </header>
