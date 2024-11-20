7a9ac326220cfd632d50324a13c088ce60ecb888
Muhammad Talha <72699916+Talha812@users.noreply.github.com>
2024-11-20 20:30:55 +0500

    Create index.php

---
+++ WordPress Basic - Enqueue Stylesheet/index.php
@@ -0,0 +1,13 @@
+<?php // include('header.php') ?>
+
+<?php get_header(); ?>
+
+<!-- Main Content Section -->
+<div id="content">
+    <h1>This is Main Content Page. Index.PHP</h1>
+    <p>This is a simple page content. You can replace it with dynamic WordPress content in the future.</p>
+</div>
+
+<?php get_footer(); ?>
+
+<?php // include('footer.php') ?>
